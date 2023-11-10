4a53c98683cc5cae35db367362da6c4f4678c1a6
jay <rameshpvb@gmail.com>
2023-11-10 20:48:25 +0800

    feat: add functionality to = button

--- calculator.js
+++ calculator.js
@@ -30,33 +30,53 @@ function addToDisplay (char) {
     displayNumber.textContent = "";
     anotherButtonPressed = true;
   }
+  if (evaluated) {
+    num1 = null;
+    displayOperation.textContent = "";
+    operationContentChanged = false;
+    evaluated = false;
+  }
   displayNumber.textContent += char;
+  num2 = +displayNumber.textContent;
 }
 
 function addToOperation (operator) {
   if (!anotherButtonPressed) {
     currentOperator = operator;
-    displayOperation.textContent = `${displayNumber.textContent} ${operator} `;
-    operationContentChanged = true;
     num1 = +displayNumber.textContent;
+    displayOperation.textContent = `${num1} ${operator} `;
+    operationContentChanged = true;
   }
   else {
     num2 = +displayNumber.textContent;
     answer = operate(currentOperator, num1, num2);
-    num1 = answer;
-    num2 = null;
+    num1 = +answer;
+    num2 = num1;
     displayNumber.textContent = answer;
     displayOperation.textContent = `${num1} ${operator} `;
     currentOperator = operator
     anotherButtonPressed = false;
   }
+  evaluated = false;
 }
 
-let num1;
-let num2;
+function evaluate () {
+  if (num1 !== null && num2 !== null) {
+    answer = operate(currentOperator, num1, num2);
+    displayOperation.textContent = `${num1} ${currentOperator} ${num2} = `;
+    displayNumber.textContent = answer;
+    num1 = answer;
+    evaluated = true;
+    anotherButtonPressed = false;
+  }
+}
+
+let num1 = null;
+let num2 = null;
 let currentOperator;
 let anotherButtonPressed = false;
 let operationContentChanged = false;
+let evaluated = false;
 const displayNumber = document.querySelector(".number");
 const displayOperation = document.querySelector(".operation");
 const numberButtons = document.querySelectorAll(".number-button:not(.equals):not(.point)");
@@ -67,3 +87,5 @@ const operatorButtons = document.querySelectorAll(".operator-button");
 for (const button of operatorButtons) {
   button.addEventListener("click", e => addToOperation(button.textContent));
 }
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", evaluate);
